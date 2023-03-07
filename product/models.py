d03c4961a3182658c82d1c6b42b8a8bbafc87911
AhmedFawzyMof <np1265448@gmail.com>
2023-03-07 05:40:34 +0200

    2nd

--- product/models.py
+++ product/models.py
@@ -52,7 +52,7 @@ class Product(models.Model):
     description   = models.TextField(blank=True, null=True)
     descriptionAr = models.TextField(blank=True, null=True)
     price         = models.IntegerField()
-    image         = models.ImageField(upload_to='products/', blank=True, null=True)
+    image         = models.CharField(max_length=1000, blank=True, null=True)
     date_added    = models.DateTimeField(auto_now_add=True)
 
     class Meta:
